2a72d98d470fa605dabb99061b22dd017a1e2e10
Subhodip100 <mindsinspiring92@gmail.com>
2026-02-22 21:00:54 +0530

    Add files via upload

---
+++ voting.py
@@ -0,0 +1,8 @@
+# Prompt the user to enter their age
+age = int(input("Enter your age: "))
+
+# Check for voting eligibility using an if-else statement
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
